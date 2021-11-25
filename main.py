4b63a58357747f7ba4118d402d77ec0c86a153bd
nuggetarian <michal.zernovic@gmail.com>
2021-11-25 15:17:33 +0100

    Pridana ikonka

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from windows.login import Login
 from tkinter import *
 from tkinter import ttk
 from ttkbootstrap import Style
+import os
+
+absolutepath = os.path.abspath(__file__)
+fileDirectory = os.path.dirname(absolutepath)
 
 style = Style('superhero')
 window = style.master
 window.title("bpc-bds-project3") 
+window.iconbitmap(fileDirectory + "\\windows\\media\\vut.ico")
 """Pridat ikonu VUT"""
 window.geometry("500x500")
-
 loginPage = Login()
+
 loginPage.loginPage(window)
 
 window.mainloop() 
