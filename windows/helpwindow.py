123c257d931f04f3fc6c33d440235a2ca410f1f5
nuggetarian <michal.zernovic@gmail.com>
2021-12-24 02:40:42 +0100

    reworked help window

--- windows/helpwindow.py
+++ windows/helpwindow.py
@@ -10,8 +10,13 @@ class Help():
     popUp = Toplevel(window)
     windowAppearance.centerWindow(popUp, 250, 250)
     popUp.title("Help")
-    infoLabel = ttk.Label(popUp, text="When adding a person: ", font='bold').pack(anchor=CENTER, pady=10)
-    cityInfoLabel = ttk.Label(popUp, text="  • City must be in database").pack(anchor="nw")
-    idInfoLabel = ttk.Label(popUp, text="  • ID must be unique").pack(anchor="nw")
+    infoLabel = ttk.Label(popUp, text="When adding a person: ", font='bold').pack(anchor=CENTER, pady=15)
+    cityInfoLabel = ttk.Label(popUp, text="  • City must be in database:").pack(anchor="nw")
+    cityInfoLabel2 = ttk.Label(popUp, text="      • Jonesboro").pack(anchor="nw")
+    cityInfoLabel2 = ttk.Label(popUp, text="      • Seattle").pack(anchor="nw")
+    cityInfoLabel2 = ttk.Label(popUp, text="      • Providence").pack(anchor="nw")
+    cityInfoLabel2 = ttk.Label(popUp, text="      • Canton").pack(anchor="nw")
+    cityInfoLabel2 = ttk.Label(popUp, text="      • Wellesley").pack(anchor="nw")
+    idInfoLabel = ttk.Label(popUp, text="  • ID must be unique").pack(anchor="nw", pady=10)
 
 
